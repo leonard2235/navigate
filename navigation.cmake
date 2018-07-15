1790a38a6cd77ea78bf282f852e1b0a80db0d23c
ChandraMohan <chandra.nair@ust-global.com>
2018-07-15 17:24:40 +0530

    navi

---
+++ navigation.cmake
@@ -0,0 +1,9 @@
+	cmake_minimum_required(VERSION 2.6)
+	add_executable(	laser_scan_publisher navigation_tutorials-indigo-devel/laser_scan_publisher_tutorial/src/laser_scan_publisher.cpp	)
+	add_executable(	odometry_publisher navigation_tutorials-indigo-devel/odometry_publisher_tutorial/src/odometry_publisher.cpp	)
+	add_executable(	point_cloud_publisher navigation_tutorials-indigo-devel/point_cloud_publisher_tutorial/src/point_cloud_publisher.cpp	)
+	add_executable(	tf_broadcaster navigation_tutorials-indigo-devel/robot_setup_tf_tutorial/src/tf_broadcaster.cpp	)
+	add_executable(	tf_listener navigation_tutorials-indigo-devel/robot_setup_tf_tutorial/src/tf_listener.cpp	)
+	add_executable(	simple_navigation_goals navigation_tutorials-indigo-devel/simple_navigation_goals_tutorial/src/simple_navigation_goals.cpp	)
+
+
